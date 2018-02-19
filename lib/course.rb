53ad00c070bbd92d6093de706403b6b2c43ffb70
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-19 15:47:44 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,13 @@
 class Course
-  attr_accessor :title, :schedule, :description 
+  attr_accessor :title, :schedule, :description
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
